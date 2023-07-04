f92da8048272f03eef73ff615049b5c8d4e0bb63
Avivcogo <avivcogo@gmail.com>
2023-07-04 13:02:48 +0200

    first step

---
+++ resources/js/Navigation/Footer/Footer-app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Footer  from './Footer.jsx'
+
+ReactDOM.createRoot(document.getElementById('Footer-app')).render(
+    <Footer />
+)
---
+++ resources/js/Navigation/Header/Header.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./Header.scss";
+
+
+
+export default function Header() 
+{
+    const [burger, setburger] = useState(false);
+    const handleClick = () => {
+        console.log(burger);
+        setburger(true)
+        
+    }
+   
+     
+    return (
+
+        
+    
+        
+        <section className="Header">
+            <div className="Header_title">
+                <img className="Logo" src="/img/bullets-g501bdc5d1_640.jpg" alt="" />
+                <h2>COD: M W 2 CAMO TRACKER</h2>
+            </div>
+            
+            {/* show above 768px, not below - CSS */}
+            <nav className="navigation">
+                <a className="Home" href="/">Home</a>
+                <a className="Register" href="#">Register</a>
+                <a className="Camos" href="#">Camos</a>
+            </nav>
+            
+            {/* show below 768px, not above - CSS */}
+            <div className="burger-icon"  onClick={handleClick}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            {/* don't show at all, only show/hide by clicking the burger icon CSS + React */}
+            {
+                 burger   // state instead of fixed value
+                ?
+                <div className="burger-menu">
+                    <a className="Home" href="/">Home</a><br />
+                    <a className="Register" href="#">Register</a><br />
+                    <a className="Camos" href="#">Camos</a><br />
+                </div>
+                : 
+                <></>
+            }
+            
+        </section>
+        
+    );
+}
