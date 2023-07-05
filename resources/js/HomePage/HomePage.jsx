cf79aeab16af3d5d9334139aec19e118073ce39a
Avivcogo <avivcogo@gmail.com>
2023-07-05 14:41:15 +0200

    homepage style

--- resources/js/HomePage/HomePage.jsx
+++ resources/js/HomePage/HomePage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Login from '../Login/Login';
 import Modal from '../Modal/Modal';
+import "../../css/HomePage.scss";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Call of Duty: MWII</h1>
-      <h1>Camo Tracker</h1>
+    <section className='HomePage'>
+    <div className='titles'>
+      <h1 className='title1'>Call of Duty: MWII</h1>
+      <h1 className='title2'>Camo Tracker</h1>
+      
       <Login />
-    </div>
+      </div>
+
+
+    </section>
   );
 };
 
