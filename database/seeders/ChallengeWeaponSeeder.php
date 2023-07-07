ddb63fdd0235ed43e5a1744ceaf7d1219326e826
GoncaloRodrigues <goncasrod2000@gmail.com>
2023-07-07 11:19:29 +0200

    fuck ups

--- database/seeders/ChallengeWeaponSeeder.php
+++ database/seeders/ChallengeWeaponSeeder.php
@@ -14,66 +14,6 @@ class ChallengeWeaponSeeder extends Seeder
         DB::table('challenge_weapon')->truncate();
         
         $challengeweapon = [
-            // AR -> M4 (Basic)
-            [
-                'challenge_id' => 1,
-                'weapon_id' => 1,
-            ],
-            [
-                'challenge_id' => 4,
-                'weapon_id' => 1,
-            ],
-            [
-                'challenge_id' => 17,
-                'weapon_id' => 1,
-            ],
-            [
-                'challenge_id' => 33,
-                'weapon_id' => 1,
-            ],
-            // AR -> M4 (Mastery)
-            [
-                'challenge_id' => 44,
-                'weapon_id' => 1,
-            ],
-            [
-                'challenge_id' => 48,
-                'weapon_id' => 1,
-            ],
-            [
-                'challenge_id' => 55,
-                'weapon_id' => 1,
-            ],
-            // AR -> TAQ-56 (Basic)
-            [
-                'challenge_id' => 1,
-                'weapon_id' => 2,
-            ],
-            [
-                'challenge_id' => 17,
-                'weapon_id' => 2,
-            ],
-            [
-                'challenge_id' => 4,
-                'weapon_id' => 2,
-            ],
-            [
-                'challenge_id' => 24,
-                'weapon_id' => 2,
-            ],
-            // AR -> TAQ-56 (Mastery)
-            [
-                'challenge_id' => 44,
-                'weapon_id' => 2,
-            ],
-            [
-                'challenge_id' => 48,
-                'weapon_id' => 2,
-            ],
-            [
-                'challenge_id' => 55,
-                'weapon_id' => 2,
-            ],
         ];
     }
 }
