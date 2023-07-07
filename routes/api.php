dd9add368c7d7e96d46a5e2484cd2a14e710a14c
Matthew Shenar <matthew.shenar@gmail.com>
2023-07-07 13:59:23 +0200

    fixed merge

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\Api\ProfileTokensController;
-=======
->>>>>>> dev/main
 use App\Http\Controllers\Api\ChallengeController;
 use App\Http\Controllers\Api\ProfileTokensController;
 use App\Http\Controllers\Api\WeaponClassController;
@@ -28,10 +24,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('profiletokens', [ProfileTokensController::class, 'index']);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> dev/main
 // Route::get('weapon', [WeaponController::class, 'index']);
 // Route::get('challenge', [ChallengeController::class, 'index']);
 Route::get('weapons', [WeaponClassController::class, 'index']);
