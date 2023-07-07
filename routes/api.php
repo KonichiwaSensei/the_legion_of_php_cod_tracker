4dbddc88101c31210a90789f272d43ce400d7629
Avivcogo <avivcogo@gmail.com>
2023-07-07 13:54:25 +0200

    merged dev/main

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\Api\ProfileTokensController;
-=======
 use App\Http\Controllers\Api\ChallengeController;
 use App\Http\Controllers\Api\WeaponClassController;
 use App\Http\Controllers\Api\WeaponController;
->>>>>>> dev/main
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,13 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
 Route::get('profiletokens', [ProfileTokensController::class, 'index']);
 
-
-
-=======
 // Route::get('weapon', [WeaponController::class, 'index']);
 // Route::get('challenge', [ChallengeController::class, 'index']);
 Route::get('weapons', [WeaponClassController::class, 'index']);
->>>>>>> dev/main
