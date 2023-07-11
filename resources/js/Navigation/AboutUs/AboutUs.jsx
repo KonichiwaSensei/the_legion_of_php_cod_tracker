cb830c6058cf072ce7e4cdc5c134034983384426
Avivcogo <avivcogo@gmail.com>
2023-07-11 12:09:02 +0200

    fixed homepage

--- resources/js/Navigation/AboutUs/AboutUs.jsx
+++ resources/js/Navigation/AboutUs/AboutUs.jsx
@@ -9,9 +9,9 @@ export default function AboutUs(){
     return (
         <>
         <section className="aboutus">
-        <AboutUsComponant imgUrl={"/img/face-gd6608c01f_640.jpg"} name={'Matthew Shenar'} linkedin={"https://www.linkedin.com/"} linkgithub={"https://www.github.com/"} />
-        <AboutUsComponant imgUrl={"/img/face-gd6608c01f_640.jpg"} name={'Goncalo Rodrigues'} linkedin={"https://www.linkedin.com/"} linkgithub={"https://www.github.com/"}/>
-        <AboutUsComponant imgUrl={"/img/face-gd6608c01f_640.jpg"} name={'Aviv Cogoluegnes'} linkedin={"https://www.linkedin.com/"} linkgithub={"https://www.github.com/"}/>
+        <AboutUsComponant imgUrl={"/img/face-gd6608c01f_640.jpg"} name={'Matthew Shenar'} linkedin={"https://www.linkedin.com/in/matthew-shenar/"} linkgithub={"https://github.com/KonichiwaSensei"} />
+        <AboutUsComponant imgUrl={"/img/face-gd6608c01f_640.jpg"} name={'Goncalo Rodrigues'} linkedin={"https://www.linkedin.com/in/goncalo-rodrigues07062000"} linkgithub={"https://github.com/GoncaloRod2000"}/>
+        <AboutUsComponant imgUrl={"/img/face-gd6608c01f_640.jpg"} name={'Aviv Cogoluegnes'} linkedin={"https://www.linkedin.com/in/aviv-cogolugenes-b58516136/"} linkgithub={"https://github.com/Avivcogo"}/>
         </section>
 
         </>
--- resources/js/Navigation/AboutUs/AboutUsComponant/AboutUsComponant.jsx
+++ resources/js/Navigation/AboutUs/AboutUsComponant/AboutUsComponant.jsx
@@ -9,8 +9,8 @@ export default function AboutUsComponant({imgUrl,name,linkedin,linkgithub}) {
                         <img className="about_us_section_img" src={imgUrl} alt="pic"/>
                         <h3 className="about_us_section_name">{name}</h3>
                         <div className="about_us_section_links_div">
-                        <a className="about_us_section_link" href={linkedin}>Linkedin</a>
-                        <a className="about_us_section_link" href={linkgithub}>GitHub</a>
+                        <a target="_blank" className="about_us_section_link" href={linkedin}>Linkedin</a>
+                        <a target="blank" className="about_us_section_link" href={linkgithub}>GitHub</a>
                         </div>
                     </div>
                 
